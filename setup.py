3a25e7971341071627d680a78608db70802f036f
movoid <bobrobotsun@163.com>
2024-05-26 20:23:10 +0800

    2024.05.26.01-movoid 更新1.0.0 1、增加一个前端显示，但是只能显示一部分信息

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='',
+    name='movoid_xml_data_pyside',
     version='1.0.0',
     packages=find_packages(),
     url='',
